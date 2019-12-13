f99bd8595465653dcff364986806ba4a731cade3
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2019-12-13 21:39:13 +0200

    Splitting Project

---
+++ RecruitmentRMIClient/src/bue_wazzuf_jobfair/BUE_Wazzuf_JobFair.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bue_wazzuf_jobfair;
+
+/**
+ *
+ * @author Amr
+ */
+public class BUE_Wazzuf_JobFair {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
